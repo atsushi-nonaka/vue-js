c7d104028f253d03a34d33fd2cddbf0088608aa0
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2020-05-02 14:05:35 +0900

    Vue.js

---
+++ src/main/java/com/example/VueJsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueJsApplication.class, args);
+	}
+
+}
